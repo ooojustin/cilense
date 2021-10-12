7897fde924854d2bb43822da62d3c8216b9e8daa
Justin <justin@garofolo.net>
2021-10-12 17:17:02 -0400

    fix model file names

--- backend/models/Base.go
+++ backend/models/Base.go
@@ -2,7 +2,7 @@ package models
 
 import "time"
 
-type BaseModel struct {
+type Base struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
