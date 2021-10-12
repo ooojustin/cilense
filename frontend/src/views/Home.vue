fcc4c445d050fb5101a9e2e16a0db8f08605c2a8
Justin <justin@garofolo.net>
2021-10-12 16:00:04 -0400

    implemented vue router w/ history mode

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <CHome />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CHome from '@/components/Home'
+
+export default {
+    name: 'Home',
+    components: {
+        CHome
+    }
+}
+</script>
