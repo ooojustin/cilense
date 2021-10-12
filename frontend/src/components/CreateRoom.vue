4802d36693fcf141e1bc55ced173e577eecb01fd
Justin <justin@garofolo.net>
2021-10-11 23:26:02 -0400

    basic createroom ui component

---
+++ frontend/src/components/CreateRoom.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-gray-700 rounded-md" style="width: 600px; height: 200px;">
+        <div class="py-5 text-xl">
+            Create Room
+        </div>
+        <input class="bg-gray-800 rounded-md py-1 px-2.5" style="width: 450px;" type="text" />
+        <br />
+        <button class="bg-blue-700 rounded-md py-2 mt-4" style="width: 450px;" type="button">
+            Generate
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CreateRoom'
+}
+</script>
+
+<style scoped>
+</style>
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        Home
+    <div class="flex justify-center h-screen" style="padding-top: 200px;">
+        <CreateRoom />
     </div>
 </template>
 
 <script>
+import CreateRoom from './CreateRoom';
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        CreateRoom
+    }
 }
 </script>
 
