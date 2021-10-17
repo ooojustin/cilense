f8976aa89513e4b79c371c57c82301daa69483c6
Justin <justin@garofolo.net>
2021-10-17 13:44:51 -0400

    remove UUIDBase, use normal id in room obj

--- backend/models/Room.go
+++ backend/models/Room.go
@@ -1,7 +1,14 @@
 package models
 
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
 type Room struct {
-	UUIDBaseModel
-	Name     string `json:"name"`
-	Password string `json:"password,omitempty"`
+	ID        uuid.UUID `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Password  string    `json:"password,omitempty"`
 }
