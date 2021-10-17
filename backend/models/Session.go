54184ce52ffd9689c89087a505423d460250be15
Justin <justin@garofolo.net>
2021-10-17 02:04:40 -0400

    fix session roomid field

--- backend/models/Session.go
+++ backend/models/Session.go
@@ -10,7 +10,7 @@ type Session struct {
 	ID        uuid.UUID `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Alias     string    `json:"alias"`
-	RoomID    string    `json:"-"`
+	RoomID    string    `json:"-" gorm:"size:36"`
 	Room      *Room     `json:"room" gorm:"foreignKey:RoomID;references:ID"`
 	IsOwner   bool      `json:"is_owner"`
 }
