bfc235730812a9b9b9d02a9087fe6e76e0a025ef
Justin <justin@garofolo.net>
2021-10-17 03:53:49 -0400

    back to using sqlite db for now

--- backend/config/database.go
+++ backend/config/database.go
@@ -2,15 +2,16 @@ package config
 
 import (
 	"cilense.co/models"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func InitDatabase() {
-	dsn := "cilense:Qpmpv$fWv3PR6P@tcp(db.cilense.co:3306)/cilense?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//dsn := "cilense:Qpmpv$fWv3PR6P@tcp(db.cilense.co:3306)/cilense?charset=utf8mb4&parseTime=True&loc=Local"
+	//database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
