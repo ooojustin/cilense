596e200c01e9ec087c46901a1c5b2eac12946a4e
Justin <justin@garofolo.net>
2021-10-17 04:57:32 -0400

    seed rand lib + comment main.go

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"cilense.co/chat"
 	"cilense.co/config"
@@ -12,21 +14,26 @@ import (
 
 func main() {
 
-	config.InitDatabase()
+	// initialize psuedo-random library with seed
+	rand.Seed(time.Now().UnixNano())
 
+	// connect to database and run goroutine to process message channel
+	config.InitDatabase()
 	go chat.MessageSender()
 
+	// configure gin router with cors middleware
 	router := gin.Default()
 	router.Use(cors.Default())
 
+	// backend index and new websocket connection handler
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "gin!")
+		c.String(http.StatusOK, "cilense.co")
 	})
-
 	router.GET("/ws", func(c *gin.Context) {
 		chat.WebSocketHandler(c.Writer, c.Request)
 	})
 
+	// room model endpoints
 	router.POST("/create_room", controllers.CreateRoom)
 	router.GET("/room/:id", controllers.GetRoom)
 
